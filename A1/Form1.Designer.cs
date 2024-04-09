b36c348367cd7870451bf84bf43fde7972271eeb
Ostapciuc Diana Miruna <163019221+dianaostapciuc@users.noreply.github.com>
2024-04-09 21:56:54 +0300

    Add files via upload

---
+++ A1/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace A1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnPrevious_Click = new System.Windows.Forms.Button();
+            this.btnNext_Click = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnNext_Parent = new System.Windows.Forms.Button();
+            this.btnPrevious_Parent = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(666, 117);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(366, 267);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS PGothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(1062, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS PGothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(1047, 268);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Date of birth";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS PGothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(1062, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Salary";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1167, 214);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1167, 263);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 33);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "connecting..";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(1088, 399);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 29);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "add!";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1167, 307);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 11;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(113, 117);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(342, 267);
+            this.dataGridView2.TabIndex = 15;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            this.dataGridView2.SelectionChanged += new System.EventHandler(this.dataGridView2_SelectionChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(40, 413);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(485, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "~ select a row from the parent table to see the children ~";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(549, 274);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 20);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "-->";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label9.Location = new System.Drawing.Point(225, 94);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(116, 20);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "parent table ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label10.Location = new System.Drawing.Point(806, 94);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(94, 20);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "child table";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("MS PGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label11.Location = new System.Drawing.Point(797, 408);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 20);
+            this.label11.TabIndex = 20;
+            // 
+            // btnPrevious_Click
+            // 
+            this.btnPrevious_Click.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnPrevious_Click.Location = new System.Drawing.Point(753, 458);
+            this.btnPrevious_Click.Name = "btnPrevious_Click";
+            this.btnPrevious_Click.Size = new System.Drawing.Size(100, 29);
+            this.btnPrevious_Click.TabIndex = 21;
+            this.btnPrevious_Click.Text = "<- previous";
+            this.btnPrevious_Click.UseVisualStyleBackColor = false;
+            this.btnPrevious_Click.Click += new System.EventHandler(this.btnPrevious_Click_Click);
+            // 
+            // btnNext_Click
+            // 
+            this.btnNext_Click.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnNext_Click.Location = new System.Drawing.Point(859, 458);
+            this.btnNext_Click.Name = "btnNext_Click";
+            this.btnNext_Click.Size = new System.Drawing.Size(100, 29);
+            this.btnNext_Click.TabIndex = 22;
+            this.btnNext_Click.Text = "next ->";
+            this.btnNext_Click.UseVisualStyleBackColor = false;
+            this.btnNext_Click.Click += new System.EventHandler(this.btnNext_Click_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnUpdate.Location = new System.Drawing.Point(1194, 399);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 29);
+            this.btnUpdate.TabIndex = 26;
+            this.btnUpdate.Text = "update!";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDelete.Location = new System.Drawing.Point(1141, 434);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 29);
+            this.btnDelete.TabIndex = 27;
+            this.btnDelete.Text = "delete!";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnNext_Parent
+            // 
+            this.btnNext_Parent.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnNext_Parent.Location = new System.Drawing.Point(160, 458);
+            this.btnNext_Parent.Name = "btnNext_Parent";
+            this.btnNext_Parent.Size = new System.Drawing.Size(100, 29);
+            this.btnNext_Parent.TabIndex = 28;
+            this.btnNext_Parent.Text = "<- previous";
+            this.btnNext_Parent.UseVisualStyleBackColor = false;
+            this.btnNext_Parent.Click += new System.EventHandler(this.btnNext_Parent_Click);
+            // 
+            // btnPrevious_Parent
+            // 
+            this.btnPrevious_Parent.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnPrevious_Parent.Location = new System.Drawing.Point(291, 458);
+            this.btnPrevious_Parent.Name = "btnPrevious_Parent";
+            this.btnPrevious_Parent.Size = new System.Drawing.Size(100, 29);
+            this.btnPrevious_Parent.TabIndex = 29;
+            this.btnPrevious_Parent.Text = "next ->";
+            this.btnPrevious_Parent.UseVisualStyleBackColor = false;
+            this.btnPrevious_Parent.Click += new System.EventHandler(this.btnPrevious_Parent_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(1047, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Position ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1167, 170);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(1323, 554);
+            this.Controls.Add(this.btnPrevious_Parent);
+            this.Controls.Add(this.btnNext_Parent);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnNext_Click);
+            this.Controls.Add(this.btnPrevious_Click);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnPrevious_Click;
+        private System.Windows.Forms.Button btnNext_Click;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnNext_Parent;
+        private System.Windows.Forms.Button btnPrevious_Parent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ A1/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Diagnostics.Contracts;
+
+namespace A1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cs = new SqlConnection("Data Source=DESKTOP-0B2ERMB\\SQLEXPRESS;Initial Catalog=hair_products;Integrated Security=True;TrustServerCertificate=true;");
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        DataSet ds_child = new DataSet();
+        BindingSource bs = new BindingSource();
+        BindingSource bs_child = new BindingSource();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            da.SelectCommand = new SqlCommand("SELECT * FROM POSITION", cs);
+            ds.Clear();
+            da.Fill(ds);
+            dataGridView2.DataSource = ds.Tables[0];
+            bs.DataSource = ds.Tables[0];
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cs.Open();
+
+                SqlCommand getLastEidCmd = new SqlCommand("SELECT MAX(eid) FROM EMPLOYEE", cs);
+                int lastEid = (int)getLastEidCmd.ExecuteScalar();
+                int newEid = lastEid + 1;
+                Random rnd = new Random();
+                int newDid = rnd.Next(1, 6);
+
+                da.InsertCommand = new SqlCommand("INSERT INTO EMPLOYEE VALUES(@eid, @oid, @did, @name, @dob, @salary)", cs);
+                da.InsertCommand.Parameters.Add("@eid", SqlDbType.Int).Value = newEid;
+                da.InsertCommand.Parameters.Add("@oid", SqlDbType.Int).Value = textBox1.Text;
+                da.InsertCommand.Parameters.Add("@did", SqlDbType.Int).Value = newDid;
+                da.InsertCommand.Parameters.Add("@name", SqlDbType.VarChar).Value = textBox3.Text;
+                da.InsertCommand.Parameters.Add("@dob", SqlDbType.Date).Value = DateTime.Parse(textBox4.Text);
+                da.InsertCommand.Parameters.Add("@salary", SqlDbType.Int).Value = Int32.Parse(textBox5.Text);
+
+                da.InsertCommand.ExecuteNonQuery();
+                MessageBox.Show("Added to the database!");
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                cs.Close();
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView2.SelectedRows.Count > 0)
+            {
+                int selectedOid = Convert.ToInt32(dataGridView2.SelectedRows[0].Cells["oid"].Value);
+
+                da.SelectCommand = new SqlCommand("SELECT * FROM EMPLOYEE WHERE oid = @selectedOid", cs);
+                da.SelectCommand.Parameters.Add("@selectedOid", SqlDbType.Int).Value = selectedOid;
+                ds_child.Clear();
+                da.Fill(ds_child);
+                bs_child.DataSource = ds_child.Tables[0];
+
+                DataView dv = new DataView(ds_child.Tables[0]);
+                dv.RowFilter = $"oid = {selectedOid}";
+                dataGridView1.DataSource = dv;
+
+
+            }
+        }
+
+        private void dataGridView1Update()
+        {
+            dataGridView1.ClearSelection();
+            dataGridView1.Rows[bs_child.Position].Selected = true;
+            records();
+        }
+
+        private void dataGridView2Update()
+        {
+            dataGridView2.ClearSelection();
+            dataGridView2.Rows[bs.Position].Selected = true;
+        }
+
+        private void records()
+        {
+            label11.Text = "Record " + (bs_child.Position+1) + " of  "
+           + bs_child.Count;
+        }
+
+        private void btnPrevious_Click_Click(object sender, EventArgs e)
+        {
+            bs_child.MovePrevious();
+            dataGridView1Update();
+            records();
+        }
+
+        private void btnNext_Click_Click(object sender, EventArgs e)
+        {
+            bs_child.MoveNext();
+            dataGridView1Update();
+            records();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int x;
+            da.UpdateCommand = new SqlCommand("Update EMPLOYEE set name = @name, date_of_birth = @dob, salary = @salary where eid = @id", cs);
+
+            try
+            {
+                da.UpdateCommand.Parameters.Add("@name", SqlDbType.VarChar).Value = textBox3.Text;
+                da.UpdateCommand.Parameters.Add("@dob", SqlDbType.Date).Value = textBox4.Text;
+                da.UpdateCommand.Parameters.Add("@salary", SqlDbType.Int).Value = textBox5.Text;
+
+                da.UpdateCommand.Parameters.Add("@id", SqlDbType.Int).Value = ds_child.Tables[0].Rows[bs_child.Position][0];
+
+            }
+            catch (Exception ex){
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            cs.Open();
+            x = da.UpdateCommand.ExecuteNonQuery();
+            cs.Close();
+            if (x >= 1)
+            {
+                MessageBox.Show("The record has been updated");
+            }
+        }
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult dr;
+            dr = MessageBox.Show("Are you sure?\n No undo after delete", "Confirm Deletion", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                da.DeleteCommand = new SqlCommand("Delete from EMPLOYEE where eid = @eid", cs);
+                da.DeleteCommand.Parameters.Add("@eid",SqlDbType.Int).Value = ds_child.Tables[0].Rows[bs_child.Position][0];
+                cs.Open();
+                da.DeleteCommand.ExecuteNonQuery();
+                cs.Close();
+                ds.Clear();
+                da.SelectCommand = new SqlCommand("SELECT * FROM POSITION", cs);
+                da.Fill(ds);
+            }
+            else
+            {
+                MessageBox.Show("Deletion Aborded");
+            }
+
+        }
+
+        private void btnNext_Parent_Click(object sender, EventArgs e)
+        {
+            bs.MovePrevious();
+            dataGridView2Update();
+        }
+
+        private void btnPrevious_Parent_Click(object sender, EventArgs e)
+        {
+            bs.MoveNext();
+            dataGridView2Update();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
